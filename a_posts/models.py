0887ee5c183b92de8bb1d967d26c3a63721ee852
Jeff Rey <jeff.rey4973@gmail.com>
2024-01-08 10:21:27 +0100

    ADD comment model to the post app

--- a_posts/models.py
+++ a_posts/models.py
@@ -33,3 +33,15 @@ class Tag(models.Model):
     
     class Meta:
         ordering = ['order']
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='comments')
+    parent_post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    body = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.CharField(max_length=100, default=uuid.uuid4, editable=False, unique=True)
+
+
+    def __str__(self):
+        return f'{self.author.username} : {self.body[:30]}'
