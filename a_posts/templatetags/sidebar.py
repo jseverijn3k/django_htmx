2c9e621b5d0d1c77ca1332e863b54988bc42496b
Jeff Rey <jeff.rey4973@gmail.com>
2024-01-14 18:17:11 +0100

    ADD top_posts and top_comments functionality to sidebar templatetag

--- a_posts/templatetags/sidebar.py
+++ a_posts/templatetags/sidebar.py
@@ -1,11 +1,12 @@
 from django.template import Library
+from django.db.models import Count
 
-from ..models import Tag
+from ..models import Tag, Post, Comment
 
 register = Library()
 
 @register.inclusion_tag('includes/sidebar.html')
-def sidebar_view():
+def sidebar_view(tag=None, user=None):
     """
     Inclusion tag for rendering the sidebar with a list of all Tag categories.
 
@@ -14,11 +15,19 @@ def sidebar_view():
     context dictionary under the key 'categories'.
 
     Usage in a template:
-    {% load your_custom_template_tags %}  <!-- Load the template tags -->
+    {% load sidebar %}  <!-- Load the template tags -->
     {% sidebar_view %}  <!-- Include the rendered sidebar in the template -->
     """
-    
+
     categories = Tag.objects.all()
-    context = { 'categories' : categories }
+    top_posts = Post.objects.annotate(num_likes=Count('likes')).filter(num_likes__gt=0).order_by('-num_likes')
+    top_comments = Comment.objects.annotate(num_likes=Count('likes')).filter(num_likes__gt=0).order_by('-num_likes')
+    context = { 
+        'categories' : categories,
+        'tag' : tag,
+        'top_posts' : top_posts,
+        'user' : user,
+        'top_comments' : top_comments,
+        }
     
     return context
